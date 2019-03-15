6476a9d4211b587a1ded5bf3050be44015671c60
Alexandru Ciocodei <alexandru.ciocodei@gmail.com>
2019-03-15 11:36:06 +0200

    Final Chapter 2 - Upload

---
+++ Chapter 2 - Primitive Data and Definite Loops/src/nestedNumbers.java
@@ -0,0 +1,22 @@
+/* Write nested for loops that produce the following output:
+ *
+ * 000111222333444555666777888999
+ * 000111222333444555666777888999
+ * 000111222333444555666777888999
+ */
+
+public class nestedNumbers
+{
+    public static void main(String[] args)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j <= 9; j++)
+            {
+                for (int k = 0; k < 3; k++)
+                    System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Chapter 2 - Primitive Data and Definite Loops/src/nestedNumbers2.java
@@ -0,0 +1,24 @@
+/* Modify your code from the previous problem to produce the following output:
+ *
+ * 99999888887777766666555554444433333222221111100000
+ * 99999888887777766666555554444433333222221111100000
+ * 99999888887777766666555554444433333222221111100000
+ * 99999888887777766666555554444433333222221111100000
+ * 99999888887777766666555554444433333222221111100000
+ */
+
+public class nestedNumbers2
+{
+    public static void main(String[] args)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 9; j >= 0; j--)
+            {
+                for (int k = 0; k < 5; k++)
+                    System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
