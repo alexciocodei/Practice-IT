e2d1c8b45a1d10fb61a65cbbce99623320364e89
Alexandru Ciocodei <alexandru.ciocodei@gmail.com>
2019-03-15 10:48:20 +0200

    Final Chapter 1

---
+++ Chapter 1 - Introduction to Java Programming/src/twoRockets.java
@@ -0,0 +1,52 @@
+/* Write a complete Java program in a class named TwoRockets that generates
+ * the following output. Use static methods to show structure and eliminate
+ * redundancy in your solution.
+ *    /\       /\
+ *   /  \     /  \
+ *  /    \   /    \
+ * +------+ +------+
+ * |      | |      |
+ * |      | |      |
+ * +------+ +------+
+ * |United| |United|
+ * |States| |States|
+ * +------+ +------+
+ * |      | |      |
+ * |      | |      |
+ * +------+ +------+
+ *    /\       /\
+ *   /  \     /  \
+ *  /    \   /    \
+ */
+public class twoRockets
+{
+    public static void main(String[] args)
+    {
+        printTriangle();
+        printSquare();
+        printUSA();
+        printSquare();
+        printTriangle();
+    }
+
+    public static void printTriangle()
+    {
+        System.out.println("   /\\       /\\");
+        System.out.println("  /  \\     /  \\");
+        System.out.println(" /    \\   /    \\");
+    }
+
+    public static void printSquare()
+    {
+        System.out.println("+------+ +------+");
+        System.out.println("|      | |      |");
+        System.out.println("|      | |      |");
+        System.out.println("+------+ +------+");
+    }
+
+    public static void printUSA()
+    {
+        System.out.println("|United| |United|");
+        System.out.println("|States| |States|");
+    }
+}
